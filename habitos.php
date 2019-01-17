1d7ac2fba3518df55aa87e5698805a5a7ccfe9a6
Ivan Salgado <ivancong10@gmail.com>
2019-01-17 09:49:24 +0100

    First commit

---
+++ habitos.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title>Hábitos</title>
+</head>
+<body>
+<h1>Hábitos</h1>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+<form name="habito" method="post" action="/habitos.php">
+<input type="text" id="nome" name="nome">
+<button id="gardar" type="submit">Gardar</button>
+</form>
+<?php
+
+// Credenciais base de datos
+$servername = "localhost";
+$username = "alan";
+$password = "Turing.2019";
+
+//crear conexiones MySQl
+$conn = mysqli_connect($servername, $username, $password, "ENIGMA");
+
+//Comprobar conexion, se falla mostrar erro
+if (!$conn) {
+die('<p>Fallou a conexion coa base de datos: </p>' . mysqli_connect_error());
+}
+echo '<p>Conexión ok!<p>';
+
+
+// Crear novo hábito (resposta ao POST)
+if (isset($_POST["nome"])){
+$insert= "INSERT INTO habitos (NOME) VALUES ('" . $_POST["nome"] . "');";
+$result = mysqli_query($conn, $insert);
+echo $result;
+echo "<p>Hábito " . $_POST["nome"] . " creado</p>";
+
+}
+
+//Borrar hábito (resposta ao GET con parámetros)
+if (isset($_REQUEST["borrar"])) {
+$delete="DELETE FROM Habitos WHERE ID=" . 
+$_REQUEST["borrar"] .";";
+$result= mysqli_query($conn, $delete);
+echo $result;
+echo "Hábito borrado";
+
+}
+
+?>
+<h1>Hábitos</h1>
+<?php
+$lectura = "SELECT * FROM habitos;";
+$habitos = mysqli_query($conn, $lectura);
+echo "Número de hábitos: " . mysqli_num_rows($habitos) . "<br>";
+while($hab = mysqli_fetch_array($habitos)){
+echo $hab['ID'] . " - " . $hab['Nome'] . "<a
+href=\"/habitos.php?borrar=" . $hab['ID'] . "\">Borrar</a><br>";
+
+}
+
+
+?>
+<p>
+
+</body>
+</html>
